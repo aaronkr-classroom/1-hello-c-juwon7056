aa65db55b622e2ab98f9784fafe9c8b542b31486
juwon7056 <juwon7056@gmail.com>
2025-09-09 16:40:52 +0900

    finished!

--- Week 2/hello.c
+++ Week 2/hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Hello World!~");
-	return 0;
+	int age = 45;
+	char grade = 'A';
+	char name[] = "Aaron"; // ['A','a','r','o','n']
+	
+	printf("Hello World!~\n");
+	printf("My name is %s.\n", name);
+	printf("I'm %d years old.\n\t", age);
+	printf("I want a %c grade.\n", grade);
+	return 0; 
 }
